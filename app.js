9d53ae188848c97d3d44517dd32c30efa20c2696
Ian McMahon <iankmcmahon@gmail.com>
2023-09-02 10:37:15 -0700

    add axios get request to script

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const $gifArea = $("#gif-area");
 const $searchInput = $("#search");
 
-async function submitHandler() {
-  const res = await axios.get("api.giphy.com/v1/gifs/search");
+async function submitHandler(apiKey, str) {
+  const res = await axios.get(
+    `api.giphy.com/v1/gifs/search", {params: {api_key:${apiKey}, q:${str}}`
+  );
+  console.log(res);
 }
